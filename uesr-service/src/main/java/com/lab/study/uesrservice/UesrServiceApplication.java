a70b963b72b29fb1a6092b95da0acbf7c7810855
LLZZHH <3177358178@qq.com>
2026-04-06 00:12:33 +0800

    项目创建1

---
+++ uesr-service/src/main/java/com/lab/study/uesrservice/UesrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lab.study.uesrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UesrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UesrServiceApplication.class, args);
+    }
+
+}
---
+++ uesr-service/src/test/java/com/lab/study/uesrservice/UesrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.study.uesrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UesrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
